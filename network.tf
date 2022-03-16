14a464a755f523a22f7bffc03b3ee40d7b6c13b8
Peggy <l4bbits@gmail.com>
2022-03-16 02:59:18 +0000

    Added network module

---
+++ network.tf
@@ -0,0 +1,8 @@
+module "network" {
+  source  = "app.terraform.io/peggyp-training1/network/azurerm"
+  version = "3.5.0"
+  # insert required variables here
+  resource_group_name = "myresourcegroup"
+
+
+}
